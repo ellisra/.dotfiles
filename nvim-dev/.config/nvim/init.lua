349c93e3b125c32e89114248017b7994ab951a8f
rob-andrew-ellis <robandrewellis@gmail.com>
2024-12-12 09:55:17 +0000

    Began working on total refactor for blink.cmp

---
+++ nvim-dev/.config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Global variables
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+
+-- Modules
+require("options") -- Vim options
+require("autocommands") -- General purpose autocommands
+require("lazy-bootstrap") -- Install lazy.nvim
+require("lazy-plugins") -- Plugins
+require("keymaps") -- Keymaps
+
+vim.cmd.colorscheme("gruvbox-material")
---
+++ nvim-dev/.config/nvim/lua/lazy-plugins.lua
@@ -0,0 +1,4 @@
+require("lazy").setup({
+    require("plugins.autopairs"),
+    require("plugins.treesitter"),
+})
