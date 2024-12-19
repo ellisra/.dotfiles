1e959d748bb08f44d2fbfd5fc474340dd09d612f
rob-andrew-ellis <robandrewellis@gmail.com>
2024-12-19 01:31:24 +0000

    Replaced floaterm plugin with custom script. Removed some plugins to move functionality to others

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -7,16 +7,53 @@ return {
             -- Better Around/Inside textobjects
             require("mini.ai").setup({ n_lines = 500 }) -- hello
 
-            -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            require("mini.surround").setup({
-                mappings = {
-                    add = "ma",
-                    delete = "md",
-                    find = "mf",
-                    find_left = "mF",
-                    highlight = "mv",
-                    replace = "mr",
-                    update_n_lines = "mn",
+            -- Which-key replacement
+            local miniclue = require("mini.clue")
+            miniclue.setup({
+                triggers = {
+                    -- Leader triggers
+                    { mode = "n", keys = "<Leader>" },
+                    { mode = "x", keys = "<Leader>" },
+
+                    -- Built-in completion
+                    { mode = "i", keys = "<C-x>" },
+
+                    -- `g` key
+                    { mode = "n", keys = "g" },
+                    { mode = "x", keys = "g" },
+
+                    -- Marks
+                    -- { mode = "n", keys = "'" },
+                    { mode = "n", keys = "`" },
+                    { mode = "x", keys = "'" },
+                    { mode = "x", keys = "`" },
+
+                    -- Registers
+                    { mode = "n", keys = "\"" },
+                    { mode = "x", keys = "\"" },
+                    { mode = "i", keys = "<C-r>" },
+                    { mode = "c", keys = "<C-r>" },
+
+                    -- Window commands
+                    { mode = "n", keys = "<C-w>" },
+
+                    -- `z` key
+                    { mode = "n", keys = "z" },
+                    { mode = "x", keys = "z" },
+                },
+
+                clues = {
+                    -- Enhance this by adding descriptions for <Leader> mapping groups
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.marks(),
+                    miniclue.gen_clues.registers(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
+                },
+
+                window = {
+                    delay = 100,
                 },
             })
 
@@ -29,8 +66,34 @@ return {
                 },
             })
 
+            -- Highlight colour codes and other specified patterns
+            require("mini.hipatterns").setup({
+                highlighters = {
+                    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+                    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+                    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+                    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+                    -- Highlight hex color strings (`#rrggbb`) using that color
+                    hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
+                },
+            })
+
             -- Replacement for nvim-autopairs
             require("mini.pairs").setup({})
+
+            -- Add/delete/replace surroundings (brackets, quotes, etc.)
+            require("mini.surround").setup({
+                mappings = {
+                    add = "ma",
+                    delete = "md",
+                    find = "mf",
+                    find_left = "mF",
+                    highlight = "mv",
+                    replace = "mr",
+                    update_n_lines = "mn",
+                },
+            })
         end,
     },
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -84,6 +84,12 @@ return {
         vim.keymap.set("n", "<leader>s.", builtin.oldfiles, {
             desc = "[S]earch Recent Files (\".\" for repeat)",
         })
+        vim.keymap.set(
+            "n",
+            "<leader>ca",
+            "<cmd>lua vim.lsp.buf.code_action()<CR>",
+            { desc = "[C]ode [A]ctions" }
+        )
         vim.keymap.set("n", "<leader><leader>", builtin.buffers, {
             desc = "[ ] Find existing buffers",
         })
