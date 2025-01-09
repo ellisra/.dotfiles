930c6549fa400fc4707c754a26d4d32e9080aa63
ellisra <robandrewellis@gmail.com>
2025-01-09 00:13:43 +0000

    Stopped markdown lsp client from attaching to scratch buffers

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -5,7 +5,7 @@ return {
     version = "v0.*",
 
     dependencies = {
-        "L3MON4D3/LuaSnip",
+        { "L3MON4D3/LuaSnip", version = "v2.*" },
         "rafamadriz/friendly-snippets",
     },
 
@@ -41,22 +41,11 @@ return {
         },
 
         snippets = {
-            expand = function(snippet)
-                require("luasnip").lsp_expand(snippet)
-            end,
-            active = function(filter)
-                if filter and filter.direction then
-                    return require("luasnip").jumpable(filter.direction)
-                end
-                return require("luasnip").in_snippet()
-            end,
-            jump = function(direction)
-                require("luasnip").jump(direction)
-            end,
+            preset = "luasnip",
         },
 
         sources = {
-            default = { "lsp", "path", "snippets", "buffer", "lazydev", "luasnip" },
+            default = { "lsp", "path", "snippets", "buffer", "lazydev" },
             providers = {
                 lazydev = {
                     name = "LazyDev",
