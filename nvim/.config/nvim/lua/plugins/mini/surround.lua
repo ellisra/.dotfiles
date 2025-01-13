49123eda028d088b944b10195dc5e19888e72a02
ellisra <robandrewellis@gmail.com>
2025-01-13 19:55:18 +0000

    Modularised mini.nvim configuration

---
+++ nvim/.config/nvim/lua/plugins/mini/surround.lua
@@ -0,0 +1,13 @@
+return {
+    require("mini.surround").setup({
+        mappings = {
+            add = "ma",
+            delete = "md",
+            find = "mf",
+            find_left = "mF",
+            highlight = "mv",
+            replace = "mr",
+            update_n_lines = "mn",
+        },
+    }),
+}
