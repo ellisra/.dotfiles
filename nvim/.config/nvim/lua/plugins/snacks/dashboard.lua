22e0a67208bbe3a175b7adde3e7352dcda7122e8
ellisra <robandrewellis@gmail.com>
2025-01-17 21:48:34 +0000

    Configuring dashboard

--- nvim/.config/nvim/lua/plugins/snacks/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/snacks/dashboard.lua
@@ -1,57 +1,65 @@
 return {
     enabled = true,
 
-    width = 90,
+    width = 60,
 
-    ---@type snacks.dashboard.Item[]
-    keys = {
-        {
-            icon = " ",
-            key = "f",
-            desc = "Find File",
-            action = ":lua Snacks.dashboard.pick('files')",
+    preset = {
+        ---@type snacks.dashboard.Item[]
+        keys = {
+            {
+                icon = " ",
+                key = "f",
+                desc = "Find File",
+                action = ":lua Snacks.dashboard.pick('files')",
+            },
+            {
+                icon = " ",
+                key = "g",
+                desc = "Grep Text",
+                action = ":lua Snacks.dashboard.pick('live_grep')",
+            },
+            {
+                icon = " ",
+                key = "r",
+                desc = "Recent Files",
+                action = ":lua Snacks.dashboard.pick('oldfiles')",
+            },
+            {
+                icon = " ",
+                key = "n",
+                desc = "Neovim Config",
+                action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            {
+                icon = "󰒲 ",
+                key = "l",
+                desc = "Lazy",
+                action = ":Lazy",
+                enabled = package.loaded.lazy ~= nil,
+            },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            header = [[⠀
+             ⢀⣴⣦⠀⠀⠀⠀⢰⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⣰⣿⣿⣿⣷⡀⠀⠀⢸⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⣿⣿⣿⣿⣿⣿⣄⠀⢸⣿⣿⡇
+            ⣿⣿⣿⠈⢿⣿⣿⣦⢸⣿⣿⡇
+            ⣿⣿⣿⠀⠀⠻⣿⣿⣿⣿⣿⡇
+            ⠹⣿⣿⠀⠀⠀⠙⣿⣿⣿⡿⠃
+            ⠀⠈⠻⠀⠀⠀⠀⠈⠿⠋⠀⠀]],
         },
-        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-        {
-            icon = " ",
-            key = "g",
-            desc = "Find Text",
-            action = ":lua Snacks.dashboard.pick('live_grep')",
-        },
-        {
-            icon = " ",
-            key = "r",
-            desc = "Recent Files",
-            action = ":lua Snacks.dashboard.pick('oldfiles')",
-        },
-        {
-            icon = " ",
-            key = "c",
-            desc = "Config",
-            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-        },
-        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-        {
-            icon = "󰒲 ",
-            key = "L",
-            desc = "Lazy",
-            action = ":Lazy",
-            enabled = package.loaded.lazy ~= nil,
-        },
-        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
     },
 
     sections = {
         { section = "header" },
-        {
-            section = "terminal",
-            cmd = "onefetch --include-hidden -d url",
-            height = 20,
-            width = 90,
-            padding = 1,
-            align = "center",
-        },
         { section = "keys", gap = 1, padding = 1 },
+        -- {
+        --     section = "terminal",
+        --     cmd = "onefetch --include-hidden -d url",
+        --     height = 22,
+        --     width = 90,
+        --     padding = 1,
+        --     align = "center",
+        -- },
         { section = "startup" },
     },
 }
