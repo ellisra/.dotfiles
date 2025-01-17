1f7cd4be513811f66c22da982b2916c0aee08ab5
rob-andrew-ellis <robandrewellis@gmail.com>
2025-01-17 13:57:06 +0000

    Modularised snacks config

---
+++ nvim/.config/nvim/lua/plugins/snacks/indent.lua
@@ -0,0 +1,14 @@
+return {
+    enabled = true,
+    priority = 1,
+    char = "▏",
+    only_scope = false,
+    only_current = false,
+    indent = { char = "▏" },
+    animate = { enabled = false },
+    scope = { enabled = false, char = "▏" },
+    blank = {
+        char = " ",
+        hl = "SnacksIndentBlank",
+    },
+}
