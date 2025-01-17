871098ae336f84067e2ae0cece43e7efd4cda5c8
ellisra <robandrewellis@gmail.com>
2025-01-17 16:08:32 +0000

    Added onefetch to dashboard

--- nvim/.config/nvim/lua/plugins/snacks/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/snacks/dashboard.lua
@@ -1,45 +1,57 @@
 return {
     enabled = true,
-    -- sections = {
-    --     { section = "header" },
-    --     {
-    --         pane = 2,
-    --         section = "terminal",
-    --         cmd = "colorscript -e square",
-    --         height = 5,
-    --         padding = 1,
-    --     },
-    --     { section = "keys", gap = 1, padding = 1 },
-    --     {
-    --         pane = 2,
-    --         icon = " ",
-    --         title = "Recent Files",
-    --         section = "recent_files",
-    --         indent = 2,
-    --         padding = 1,
-    --     },
-    --     {
-    --         pane = 2,
-    --         icon = " ",
-    --         title = "Projects",
-    --         section = "projects",
-    --         indent = 2,
-    --         padding = 1,
-    --     },
-    --     {
-    --         pane = 2,
-    --         icon = " ",
-    --         title = "Git Status",
-    --         section = "terminal",
-    --         enabled = function()
-    --             return Snacks.git.get_root() ~= nil
-    --         end,
-    --         cmd = "git status --short --branch --renames",
-    --         height = 5,
-    --         padding = 1,
-    --         ttl = 5 * 60,
-    --         indent = 3,
-    --     },
-    --     { section = "startup" },
-    -- },
+
+    width = 90,
+
+    ---@type snacks.dashboard.Item[]
+    keys = {
+        {
+            icon = " ",
+            key = "f",
+            desc = "Find File",
+            action = ":lua Snacks.dashboard.pick('files')",
+        },
+        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+        {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = ":lua Snacks.dashboard.pick('live_grep')",
+        },
+        {
+            icon = " ",
+            key = "r",
+            desc = "Recent Files",
+            action = ":lua Snacks.dashboard.pick('oldfiles')",
+        },
+        {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+        },
+        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+        {
+            icon = "󰒲 ",
+            key = "L",
+            desc = "Lazy",
+            action = ":Lazy",
+            enabled = package.loaded.lazy ~= nil,
+        },
+        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    },
+
+    sections = {
+        { section = "header" },
+        {
+            section = "terminal",
+            cmd = "onefetch --include-hidden -d url",
+            height = 20,
+            width = 90,
+            padding = 1,
+            align = "center",
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+    },
 }
