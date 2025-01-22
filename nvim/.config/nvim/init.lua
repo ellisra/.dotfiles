63df259d8738934513803c4c5fbe7347f6db2066
ellisra <robandrewellis@gmail.com>
2025-01-22 16:33:33 +0000

    Fixed bug with `:WeeklyRecap` user command accidentaly firing

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.g.have_nerd_font = true
 -- Modules
 require("options") -- Vim options
 require("autocommands") -- General purpose autocommands
+require("user-commands") -- User commands
 require("lazy-bootstrap") -- Install lazy.nvim
 require("lazy-plugins") -- Plugins
 require("keymaps") -- Keymaps
