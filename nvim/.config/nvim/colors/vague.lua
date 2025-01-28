36bc3db6f284c2415189ad6ee24305af2d8d93d6
ellisra <robandrewellis@gmail.com>
2025-01-28 14:26:59 +0000

    Updated vague and habamax

--- nvim/.config/nvim/colors/vague.lua
+++ nvim/.config/nvim/colors/vague.lua
@@ -1,17 +1,17 @@
 require("base16-colorscheme").setup({
-    base00 = "#18191a", -- bg0
-    base01 = "#282828", -- bg1
-    base02 = "#3c3836", -- bg2
-    base03 = "#504945", -- bg3
-    base04 = "#928374", -- fg3
-    base05 = "#9893a5", -- fg2
-    base06 = "#908caa", -- fg1
+    base00 = "#2a2c2e", -- bg0
+    base01 = "#313336", -- bg1
+    base02 = "#4a4c4f", -- bg2
+    base03 = "#4a4c4f", -- bg3
+    base04 = "#a2a1b3", -- fg3
+    base05 = "#b9b8cc", -- fg2
+    base06 = "#cecce0", -- fg1
     base07 = "#e0def4", -- fg0
     base08 = "#d2788c", -- red
     base09 = "#d2a374", -- orange
     base0A = "#e6be8c", -- yellow
     base0B = "#8faf77", -- green
-    base0C = "#c7c7d4", -- aqua
+    base0C = "#bdd4ba", -- aqua
     base0D = "#bad1ce", -- blue
     base0E = "#b9a3ba", -- purple
     base0F = "#be8c8c", -- bonus
