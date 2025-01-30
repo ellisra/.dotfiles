e4570ef3f27356546e788d4ae4089aaa16f5c0f6
ellisra <robandrewellis@gmail.com>
2025-01-30 00:05:01 +0000

    Added oldworld

---
+++ nvim/.config/nvim/colors/oldworld.lua
@@ -0,0 +1,18 @@
+require("base16-colorscheme").setup({
+    base00 = "#252526", -- bg0
+    base01 = "#313133", -- bg1
+    base02 = "#3d3d40", -- bg2
+    base03 = "#49494d", -- bg3
+    base04 = "#a2a1a6", -- fg3
+    base05 = "#afadb3", -- fg2
+    base06 = "#bbbabf", -- fg1
+    base07 = "#c9c7cd", -- fg0
+    base08 = "#ea83a5", -- red
+    base09 = "#f5a191", -- orange
+    base0A = "#e6b99d", -- yellow
+    base0B = "#90b99f", -- green
+    base0C = "#85b5ba", -- aqua
+    base0D = "#92a2d5", -- blue
+    base0E = "#e29eca", -- purple
+    base0F = "#be8c8c", -- bonus
+})
