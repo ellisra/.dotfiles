1bb975aae83c9630bda6fa08c9a1e72164b5e28d
ellisra <robandrewellis@gmail.com>
2025-01-30 16:26:22 +0000

    Improved pattern matching for mini.hipatterns

--- nvim/.config/nvim/colors/oldworld.lua
+++ nvim/.config/nvim/colors/oldworld.lua
@@ -2,9 +2,9 @@ require("base16-colorscheme").setup({
     base00 = "#252526", -- bg0
     base01 = "#313133", -- bg1
     base02 = "#3d3d40", -- bg2
-    base03 = "#49494d", -- bg3
+    base03 = "#797980", -- bg3
     base04 = "#a2a1a6", -- fg3
-    base05 = "#afadb3", -- fg2
+    base05 = "#c9c7cd", -- fg2
     base06 = "#bbbabf", -- fg1
     base07 = "#c9c7cd", -- fg0
     base08 = "#ea83a5", -- red
--- nvim/.config/nvim/lua/plugins/mini/hipatterns.lua
+++ nvim/.config/nvim/lua/plugins/mini/hipatterns.lua
@@ -1,11 +1,28 @@
+local function in_comment(marker)
+    -- Add language comment syntax here as needed
+    local comment_tbl = {
+        python = "#",
+        lua = "--",
+    }
+
+    return function(bufnr)
+        local comment = comment_tbl[vim.bo[bufnr].filetype]
+        if comment == nil then
+            return nil
+        end
+
+        return comment .. " ()" .. marker .. "()%f[%W]"
+    end
+end
+
 return {
     require("mini.hipatterns").setup({
         highlighters = {
-            fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-            hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-            note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+            fixme = { pattern = in_comment("FIXME"), group = "MiniHipatternsFixme" },
+            hack = { pattern = in_comment("HACK"), group = "MiniHipatternsHack" },
+            todo = { pattern = in_comment("TODO"), group = "MiniHipatternsTodo" },
+            note = { pattern = in_comment("NOTE"), group = "MiniHipatternsNote" },
 
-            -- Highlight hex color strings (`#rrggbb`) using that color
             hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
         },
     }),
