c9f37d06a457fc8cbb9c64ad2c41a4ec15bff7b2
ellisra <robandrewellis@gmail.com>
2025-02-22 18:08:39 +0000

    Added some new themes

---
+++ nvim/.config/nvim/colors/melange.lua
@@ -0,0 +1,18 @@
+require("base16-colorscheme").setup({
+    base00 = "#292522", -- bg0
+    base01 = "#34302C", -- bg1
+    base02 = "#403A36", -- bg2
+    base03 = "#867462", -- bg3
+    base04 = "#C1A78E", -- fg3
+    base05 = "#ECE1D7", -- fg2
+    base06 = "#ECE1D7", -- fg1
+    base07 = "#ECE1D7", -- fg0
+    base08 = "#D47766", -- red
+    base09 = "#E49B5D", -- orange
+    base0A = "#EBC06d", -- yellow
+    base0B = "#85b695", -- green
+    base0C = "#89b3b6", -- aqua
+    base0D = "#a3a9ce", -- blue
+    base0E = "#cf9bc2", -- purple
+    base0F = "#aaaaaa", -- bonus
+})
