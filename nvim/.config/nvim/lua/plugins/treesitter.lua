6ad8b1fbe44b3b2d76db5df70eb2d603b1d06685
ellisra <robandrewellis@gmail.com>
2025-05-30 14:36:56 +0100

    Migrated to main branch of nvim-treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,30 +2,40 @@
 return {
     "nvim-treesitter/nvim-treesitter",
 
+    lazy = false,
+    branch = "main",
     build = ":TSUpdate",
 
-    opts = {
-        ensure_installed = {
+    config = function()
+        local patterns = {
             "bash",
             "c",
             "diff",
             "html",
+            "json",
             "lua",
             "luadoc",
             "markdown",
             "markdown_inline",
             "query",
+            "regex",
             "vim",
             "vimdoc",
             "rust",
+            "toml",
             "python",
-        },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-    },
+        }
 
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
+        require("nvim-treesitter").install(patterns)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = patterns,
+            callback = function()
+                vim.treesitter.start()
+                vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                vim.bo.indentexpr =
+                    "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
     end,
 }
