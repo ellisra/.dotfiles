a5d4f75a47550a1f6c8a0ec10dff073a99a16585
ellisra <robandrewellis@gmail.com>
2025-06-03 23:57:56 +0100

    Overwrote obsidian frontmatter function to remove aliases by default

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -66,6 +66,22 @@ return {
             vim.ui.open(url)
         end,
 
+        note_frontmatter_func = function(note)
+            local out = { id = note.id, tags = note.tags }
+
+            if note.aliases ~= nil and not vim.tbl_isempty(note.aliases) then
+                out.aliases = note.aliases
+            end
+
+            if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+                for k, v in pairs(note.metadata) do
+                    out[k] = v
+                end
+            end
+
+            return out
+        end,
+
         picker = {
             name = "fzf-lua",
             note_mappings = {
