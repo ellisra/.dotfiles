76b205a4ffb8616f2dbb6dc6605455695ef18e82
ellisra <robandrewellis@gmail.com>
2025-06-27 16:13:51 +0100

    WIP table align keymap

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -20,6 +20,7 @@ vim.o.clipboard = "unnamedplus" -- Sync yank register with system clipboard
 vim.o.confirm = true -- Ask if you want to save unsaved changes on :q or :e
 vim.o.cursorline = true -- Highlight cursor line
 vim.o.ignorecase = true -- Ignore case sensitivity in search
+vim.o.lazyredraw = true
 vim.o.linebreak = true -- Wrap lines on spaces rather than characters
 vim.o.mouse = "a" -- Allows mouse mode
 vim.o.scrolloff = 10 -- Minimum number of lines above or below the cursor
