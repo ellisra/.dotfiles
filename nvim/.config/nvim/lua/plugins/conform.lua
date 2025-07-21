899ff05a405daa32c1cb27548aaa9d0b6094b5c0
rellis-protector <robert.ellis@protectorinsurance.co.uk>
2025-07-21 16:58:16 +0100

    Disabled `conform.nvim` formatting for python

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -12,11 +12,11 @@ return {
         },
         formatters_by_ft = {
             lua = { "stylua" },
-            python = {
-                "ruff_fix",
-                "ruff_format",
-                "ruff_organize_imports",
-            },
+            -- python = {
+            --     "ruff_fix",
+            --     "ruff_format",
+            --     "ruff_organize_imports",
+            -- },
             go = { "goimports", "gofmt" },
         },
     },
