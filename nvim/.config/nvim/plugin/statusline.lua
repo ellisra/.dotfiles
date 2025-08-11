bec711ae8ec734edba37ff47717e7e2a023b2771
ellisra <robandrewellis@gmail.com>
2025-08-11 17:06:20 +0100

    Rewrote obsidian.nvim configuration
    Rewrites `obsidian.nvim` configuration to use new `Note` API. Also adds
    a backlink count to the statusline in markdown buffers.

--- nvim/.config/nvim/plugin/statusline.lua
+++ nvim/.config/nvim/plugin/statusline.lua
@@ -122,7 +122,13 @@ end
 
 local function md_info()
     if vim.bo.filetype == "markdown" then
-        return string.format("%s words ", vim.fn.wordcount().words)
+        return string.format(
+            "%s words %s   ",
+            vim.fn.wordcount().words,
+            #require("obsidian.search").find_backlinks(
+                require("obsidian.api").current_note(0)
+            )
+        )
     end
 
     return ""
