401cc6d5c07b0646df3b086cb7105c0ce19a7cd6
ellisra <robandrewellis@gmail.com>
2025-09-03 22:29:23 +0100

    Updated fyler config

--- nvim/.config/nvim/lua/plugins/fyler.lua
+++ nvim/.config/nvim/lua/plugins/fyler.lua
@@ -2,57 +2,38 @@ return {
     "A7Lavinraj/fyler.nvim",
 
     opts = {
-        views = {
-            confirm = {
-                win = {
-                    kind = "float",
-                    kind_presets = {
-                        float = {
-                            height = "0.2rel",
-                            width = "0.2rel",
-                            top = "0.35rel",
-                            left = "0.4rel",
-                        },
-                    },
-                    win_opts = {
-                        winhighlight = "Normal:FzfLuaNormal,FloatBorder:FzfLuaBorder,FloatTitle:FzfLuaTitle",
-                    },
+        confirm_simple = true,
+        default_explorer = true,
+        indentscope = {
+            marker = "▏",
+        },
+
+        win = {
+            border = "single",
+            buf_opts = {
+                buflisted = false,
+                buftype = "acwrite",
+                expandtab = true,
+                filetype = "fyler",
+                shiftwidth = 2,
+                syntax = "fyler",
+            },
+            kind = "float",
+            kind_presets = {
+                split_left_most = {
+                    width = "0.12rel",
+                },
+                split_right_most = {
+                    width = "0.15rel",
+                },
+                float = {
+                    width = "0.3rel",
+                    top = "0.1rel",
+                    left = "0.35rel",
                 },
             },
-            explorer = {
-                confirm_simple = true,
-                default_explorer = true,
-                indentscope = {
-                    marker = "▏",
-                },
-                win = {
-                    border = "single",
-                    buf_opts = {
-                        buflisted = false,
-                        buftype = "acwrite",
-                        expandtab = true,
-                        filetype = "fyler",
-                        shiftwidth = 2,
-                        syntax = "fyler",
-                    },
-                    kind = "split_right_most",
-                    kind_presets = {
-                        split_left_most = {
-                            width = "0.12rel",
-                        },
-                        split_right_most = {
-                            width = "0.15rel",
-                        },
-                        float = {
-                            width = "0.3rel",
-                            top = "0.1rel",
-                            left = "0.35rel",
-                        },
-                    },
-                    win_opts = {
-                        winhighlight = "FzfLuaNormal:FzfLuaNormal,FzfLuaBorder:FzfLuaBorder,FzfLuaTitle:FzfLuaTitle",
-                    },
-                },
+            win_opts = {
+                winhighlight = "FzfLuaNormal:FzfLuaNormal,FzfLuaBorder:FzfLuaBorder,FzfLuaTitle:FzfLuaTitle",
             },
         },
     },
@@ -61,13 +42,7 @@ return {
         local Fyler = require("fyler")
 
         vim.keymap.set("n", "<leader>o", function()
-            local window_width = vim.fn.winwidth(0)
-
-            -- if window_width >= 180 then
-            --     Fyler.open()
-            -- else
             Fyler.open({ kind = "float" })
-            -- end
         end, { desc = "[O]pen File Tree" })
 
         vim.api.nvim_create_autocmd("FileType", {
