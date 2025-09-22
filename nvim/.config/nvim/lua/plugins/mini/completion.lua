a6ba67026fcee386a4de03b1695749ec876bcb0d
ellisra <robandrewellis@gmail.com>
2025-09-22 14:21:42 +0100

    Replaced `blink.cmp` with `mini.completion`

---
+++ nvim/.config/nvim/lua/plugins/mini/completion.lua
@@ -0,0 +1,22 @@
+local function truncate_items(items, _)
+    local max_len = 30
+    for _, item in ipairs(items) do
+        if item.label and #item.label > max_len then
+            item.label = item.label:sub(1, max_len) .. "…"
+        end
+        if item.labelDetails and item.labelDetails.detail then
+            item.labelDetails.detail = nil
+        end
+        item.detail = nil
+    end
+
+    return items
+end
+
+return {
+    require("mini.completion").setup({
+        delay = { completion = 50 },
+        fallback_action = "",
+        lsp_completion = { process_items = truncate_items },
+    }),
+}
