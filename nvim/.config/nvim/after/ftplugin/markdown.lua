3d7af370acd1e0f7f98aa0211f651cf7ac56af2e
ellisra <robandrewellis@gmail.com>
2025-10-25 16:43:02 +0100

    Moved ftplugin and lsp configs into after/

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,10 +1,6 @@
-vim.opt_local.breakindent = true
-vim.opt_local.linebreak = true
 vim.opt_local.wrap = true
 vim.opt_local.spell = true
 
-local utils = require('utils')
-
 vim.keymap.set('n', '-', function()
-    utils.toggle_checkbox()
+    require('utils').toggle_checkbox()
 end, { desc = 'Togge Checkbox', buffer = true })
