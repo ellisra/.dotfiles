647356ef1df399950719b92cc6c4019fa8fe6a3f
ellisra <robandrewellis@gmail.com>
2025-10-25 16:43:02 +0100

    Moved ftplugin and lsp configs into after/

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -14,24 +14,28 @@ vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.tabstop = 4
 
--- General
-vim.o.clipboard = 'unnamedplus' -- Sync yank register with system clipboard
-vim.o.confirm = true -- Ask if you want to save unsaved changes on :q or :e
-vim.o.cursorline = true -- Highlight cursor line
-vim.o.ignorecase = true -- Ignore case sensitivity in search
-vim.o.lazyredraw = true
-vim.o.mouse = 'a' -- Allows mouse mode
-vim.o.scrolloff = 10 -- Minimum number of lines above or below the cursor
-vim.o.showmode = false -- Display mode visual since I'm using a bar
-vim.o.signcolumn = 'yes:1' -- Make sign column on by default
-vim.o.smartcase = true -- Ignore case sensitivity in search
-vim.o.spelllang = 'en_gb' -- Sets spelling language to british english
-vim.o.spellcapcheck = ''
-vim.o.splitbelow = true -- Default horizontal split direction
-vim.o.splitright = true -- Default vertical split direction
-vim.o.undofile = true -- Save undo history
-vim.o.undolevels = 1000
-vim.o.updatetime = 250 -- Decreases update time
-vim.o.wrap = false
+-- Completion
 vim.o.completeopt = 'menuone,noselect,fuzzy'
 vim.o.pumheight = 10
+
+-- General
+vim.o.breakindent = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.confirm = true
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.lazyredraw = true
+vim.o.linebreak = true
+vim.o.mouse = 'a'
+vim.o.scrolloff = 10
+vim.o.showmode = false
+vim.o.signcolumn = 'yes:1'
+vim.o.smartcase = true
+vim.o.spelllang = 'en_gb'
+vim.o.spellcapcheck = ''
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.undofile = true
+vim.o.undolevels = 1000
+vim.o.updatetime = 250
+vim.o.wrap = false
