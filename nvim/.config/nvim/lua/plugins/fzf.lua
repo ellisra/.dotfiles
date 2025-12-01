bbbda5d76275abeee086ef1cc23df1e8a22ddd6a
ellisra <robandrewellis@gmail.com>
2025-12-01 00:02:54 +0000

    Replaced lazy.nvim with vim.pack

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,220 +1,195 @@
 local constants = require('constants')
 
-return {
-    'ibhagwan/fzf-lua',
+local FzfLua = require('fzf-lua')
+local utils = require('utils')
+local keymap = vim.keymap.set
 
-    lazy = false,
-    dependencies = 'nvim-mini/mini.nvim',
+FzfLua.setup({
+    ui_select = true,
+    fzf_colors = true,
+    file_icon_padding = ' ',
+    oldfiles = { include_current_session = true },
+    defaults = { formatter = 'path.filename_first', git_icons = true },
 
-    opts = {
-        ui_select = true,
-        fzf_colors = true,
-        file_icon_padding = ' ',
-        oldfiles = { include_current_session = true },
-        defaults = { formatter = 'path.filename_first', git_icons = true },
-
-        keymap = {
-            builtin = {
-                ['<C-p>'] = 'toggle-preview',
-                ['<S-Down>'] = 'preview-page-down',
-                ['<S-Up>'] = 'preview-page-up',
-            },
+    keymap = {
+        builtin = {
+            ['<C-p>'] = 'toggle-preview',
+            ['<S-Down>'] = 'preview-page-down',
+            ['<S-Up>'] = 'preview-page-up',
         },
+    },
+
+    winopts = {
+        backdrop = 100,
+        height = 0.50,
+        width = 0.50,
+        row = 1,
+        col = 0,
+
+        preview = {
+            wrap = true,
+            vertical = 'down:90%',
+            hidden = true,
+        },
+    },
+
+    grep = {
+        rg_opts = '--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --hidden -g "!.git" -e',
 
         winopts = {
-            backdrop = 100,
-            height = 0.50,
-            width = 0.50,
+            height = 0.5,
+            width = 1,
             row = 1,
             col = 0,
 
             preview = {
                 wrap = true,
-                vertical = 'down:90%',
-                hidden = true,
-            },
-        },
-
-        grep = {
-            rg_opts = '--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --hidden -g "!.git" -e',
-
-            winopts = {
-                height = 0.5,
-                width = 1,
-                row = 1,
-                col = 0,
-
-                preview = {
-                    wrap = true,
-                    horizontal = 'right:70%',
-                    hidden = false,
-                },
+                horizontal = 'right:70%',
+                hidden = false,
             },
         },
     },
+})
 
-    init = function()
-        local FzfLua = require('fzf-lua')
-        local utils = require('utils')
-        local keymap = vim.keymap.set
+keymap('n', '<leader>sf', function()
+    FzfLua.files()
+end, { desc = '[S]earch [F]iles' })
 
-        -- Buffers and Files
-        keymap('n', '<leader>sf', function()
-            FzfLua.files()
-        end, { desc = '[S]earch [F]iles' })
+keymap('n', '<leader>so', function()
+    FzfLua.oldfiles()
+end, { desc = '[S]earch [O]ldfiles' })
 
-        keymap('n', '<leader>so', function()
-            FzfLua.oldfiles()
-        end, { desc = '[S]earch [O]ldfiles' })
+keymap('n', '<leader>sn', function()
+    FzfLua.files({ cwd = vim.fn.stdpath('config') })
+end, { desc = '[S]earch [N]eovim config' })
 
-        keymap('n', '<leader>sn', function()
-            FzfLua.files({ cwd = vim.fn.stdpath('config') })
-        end, { desc = '[S]earch [N]eovim config' })
+keymap('n', '<leader><leader>', function()
+    FzfLua.resume()
+end, { desc = '[ ] Resume previous search' })
 
-        keymap('n', '<leader><leader>', function()
-            FzfLua.resume()
-        end, { desc = '[ ] Resume previous search' })
+keymap('n', '<leader>sj', function()
+    FzfLua.jumps({
+        winopts = {
+            width = 1,
+            preview = {
+                hidden = false,
+                layout = 'horizontal',
+                horizontal = 'right:60%',
+            }
+        }
+    })
+end, { desc = '[S]earch [J]umps' })
 
-        keymap('n', '<leader>sj', function()
-            FzfLua.jumps({
-                winopts = {
-                    width = 1,
-                    preview = {
-                        hidden = false,
-                        layout = 'horizontal',
-                        horizontal = 'right:60%',
-                    }
-                }
-            })
-        end, { desc = '[S]earch [J]umps' })
+keymap('n', '<leader>sb', function()
+    FzfLua.buffers()
+end, { desc = '[S]earch [B]uffers' })
 
-        keymap('n', '<leader>sb', function()
-            FzfLua.buffers()
-        end, { desc = '[S]earch [B]uffers' })
+keymap('n', '<leader>sg', function()
+    FzfLua.live_grep()
+end, { desc = '[S]earch [G]rep' })
 
-        -- Grep
-        keymap('n', '<leader>sg', function()
-            FzfLua.live_grep()
-        end, { desc = '[S]earch [G]rep' })
+keymap('n', '<leader>sd', function()
+    FzfLua.diagnostics_document({
+        winopts = {
+            width = 1,
+            height = 0.2,
+        },
+    })
+end, { desc = '[S]earch [D]iagnostics' })
 
-        -- LSP and Diagnostics
-        keymap('n', '<leader>sd', function()
-            FzfLua.diagnostics_document({
-                winopts = {
-                    width = 1,
-                    height = 0.2,
-                },
-            })
-        end, { desc = '[S]earch [D]iagnostics' })
+keymap('n', '<leader>sr', function()
+    FzfLua.lsp_references({
+        winopts = {
+            width = 1,
+            height = 0.2,
+            preview = {
+                hidden = false,
+                layout = 'horizontal',
+                horizontal = 'right:60%',
+            },
+        }
+    })
+end)
 
-        keymap('n', '<leader>sr', function()
-            FzfLua.lsp_references({
-                winopts = {
-                    width = 1,
-                    height = 0.2,
-                    preview = {
-                        hidden = false,
-                        layout = 'horizontal',
-                        horizontal = 'right:60%',
-                    },
-                }
-            })
-        end)
+keymap('n', '<leader>ca', function()
+    FzfLua.lsp_code_actions({
+        winopts = {
+            width = 1,
+            preview = {
+                hidden = false,
+                layout = 'horizontal',
+                horizontal = 'right:70%'
+            }
+        }
+    })
+end, { desc = '[C]ode [A]ctions' })
 
-        keymap('n', '<leader>sD', function()
-            FzfLua.diagnostics_workspace({
-                winopts = {
-                    width = 1,
-                    height = 0.2,
-                },
-            })
-        end, { desc = '[S]earch workspace [D]iagnostics' })
+keymap('n', 'gd', function()
+    FzfLua.lsp_definitions({ jump1 = true })
+end, { desc = '[G]o to [D]efinition' })
 
-        keymap('n', '<leader>ca', function()
-            FzfLua.lsp_code_actions({
-                winopts = {
-                    width = 1,
-                    preview = {
-                        hidden = false,
-                        layout = 'horizontal',
-                        horizontal = 'right:70%'
-                    }
-                }
-            })
-        end, { desc = '[C]ode [A]ctions' })
+keymap('n', '<leader>sm', function()
+    FzfLua.helptags({
+        winopts = {
+            width = 1,
+            preview = {
+                hidden = false,
+                layout = 'horizontal',
+                horizontal = 'right:60%',
+            },
+        },
+    })
+end, { desc = '[S]earch [M]anual' })
 
-        keymap('n', 'gd', function()
-            FzfLua.lsp_definitions({ jump1 = true })
-        end, { desc = '[G]o to [D]efinition' })
+keymap('n', '<leader>sk', function()
+    FzfLua.keymaps({
+        winopts = {
+            width = 1,
+            height = 0.4,
+            preview = {
+                layout = 'vertical',
+                vertical = 'down:40%',
+            },
+        },
+    })
+end, { desc = '[S]earch [K]eymaps' })
 
-        -- Misc
-        keymap('n', '<leader>sm', function()
-            FzfLua.helptags({
-                winopts = {
-                    width = 1,
-                    preview = {
-                        hidden = false,
-                        layout = 'horizontal',
-                        horizontal = 'right:60%',
-                    },
-                },
-            })
-        end, { desc = '[S]earch [M]anual' })
+keymap('n', '<leader>sc', function()
+    FzfLua.colorschemes({ winopts = { height = 0.2, width = 0.2 } })
+end, { desc = '[S]earch [C]olorschemes' })
 
-        keymap('n', '<leader>sk', function()
-            FzfLua.keymaps({
-                winopts = {
-                    width = 1,
-                    height = 0.4,
-                    preview = {
-                        layout = 'vertical',
-                        vertical = 'down:40%',
-                    },
-                },
-            })
-        end, { desc = '[S]earch [K]eymaps' })
+keymap('n', '<leader>sh', function()
+    FzfLua.highlights({ fzf_colors = true })
+end, { desc = '[S]earch [H]ighlights' })
 
-        keymap('n', '<leader>sc', function()
-            FzfLua.colorschemes({
-                winopts = { height = 0.2, width = 0.2 },
-            })
-        end, { desc = '[S]earch [C]olorschemes' })
+keymap('n', '<leader>sp', function()
+    FzfLua.spell_suggest({ winopts = { width = 0.1, height = 0.2 } })
+end, { desc = '[S][p]elling suggestions' })
 
-        keymap('n', '<leader>sh', function()
-            FzfLua.highlights({ fzf_colors = true })
-        end, { desc = '[S]earch [H]ighlights' })
+keymap('n', '<leader>cp', function()
+    FzfLua.complete_path()
+end, { desc = '[C]omplete [P]ath' })
 
-        keymap('n', '<leader>sp', function()
-            FzfLua.spell_suggest({ winopts = { width = 0.1, height = 0.2 } })
-        end, { desc = '[S][p]elling suggestions' })
-
-        -- Completion
-        keymap('n', '<leader>cp', function()
-            FzfLua.complete_path()
-        end, { desc = '[C]omplete [P]ath' })
-
-        keymap('n', '<leader>tm', function()
-            local template_dir = vim.fn.expand(constants.MD_TEMPLATE_DIR)
-            local p = vim.loop.fs_scandir(template_dir)
-            if not p then return end
-            local entries = {}
-            while true do
-                local name, type = vim.loop.fs_scandir_next(p)
-                if not name then break end
-                if type == 'file' then
-                    table.insert(entries, name)
-                end
-            end
-            FzfLua.fzf_exec(entries, {
-                actions = {
-                    ['default'] = function(selected)
-                        local choice = selected[1]
-                        if not choice then return end
-                        local path = template_dir .. choice
-                        utils.insert_template({ path=path, filename=utils.get_current_filename() })
-                    end,
-                    },
-                })
-        end, { desc = '[T]e[M]plate' })
-    end,
-}
+keymap('n', '<leader>tm', function()
+    local template_dir = vim.fn.expand(constants.MD_TEMPLATE_DIR)
+    local p = vim.loop.fs_scandir(template_dir)
+    if not p then return end
+    local entries = {}
+    while true do
+        local name, type = vim.loop.fs_scandir_next(p)
+        if not name then break end
+        if type == 'file' then
+            table.insert(entries, name)
+        end
+    end
+    FzfLua.fzf_exec(entries, {
+        actions = {
+            ['default'] = function(selected)
+                local choice = selected[1]
+                if not choice then return end
+                local path = template_dir .. choice
+                utils.insert_template({ path=path, filename=utils.get_current_filename() })
+            end,
+            },
+        })
+end, { desc = '[T]e[M]plate' })
--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,25 +1,14 @@
-return {
-    'nvim-mini/mini.nvim',
+require('mini.ai').setup()
+require('mini.align').setup()
+require('mini.icons').setup()
+require('mini.jump').setup()
+require('mini.splitjoin').setup()
+require('mini.pairs').setup({ modes = { insert = true, command = true } })
 
-    lazy = false,
-    config = function()
-        -- One-liners
-        require('mini.ai').setup()
-        require('mini.align').setup()
-        require('mini.icons').setup()
-        require('mini.jump').setup()
-        require('mini.pairs').setup({ modes = { insert = true, command = true } })
-        require('mini.splitjoin').setup()
-        require('mini.surround').setup()
+require('plugins.mini-clue')
+require('plugins.mini-diff')
+require('plugins.mini-hipatterns')
+require('plugins.mini-indentscope')
+require('plugins.mini-keymap')
 
-        -- Configured Modules
-        require('plugins.mini-clue')
-        require('plugins.mini-diff')
-        require('plugins.mini-hipatterns')
-        require('plugins.mini-indentscope')
-        require('plugins.mini-keymap')
-
-        -- Mics Functions
-        require('mini.misc').setup_termbg_sync()
-    end,
-}
+require('mini.misc').setup_termbg_sync()
