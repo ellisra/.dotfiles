808a0d6aad70a0d9f6cc81de9a1a0d0689907668
ellisra <robandrewellis@gmail.com>
2025-12-09 20:11:24 +0000

    Disabled snippets as far as possible globally

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -65,6 +65,11 @@ return {
                     return 1
                 end
             end,
+            transform_items = function(_, items)
+                return vim.tbl_filter(function(item)
+                    return item.kind ~= require('blink.cmp.types').CompletionItemKind.Snippet
+                end, items)
+            end
         },
     },
 
