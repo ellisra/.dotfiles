7cf70f24eb85bb9a0d94b498a1ce9429c109b7dd
ellisra <robandrewellis@gmail.com>
2026-01-04 21:43:35 +0000

    Added darkearth

---
+++ nvim/.config/nvim/lua/palettes/darkearth.lua
@@ -0,0 +1,51 @@
+local palette = {
+    background = '#1f1b19',
+    cursorline = '#1a1714',
+    visual     = '#3B3330',
+    comment    = '#6E665E',
+    argument   = '#b3a16d',
+    delimeter  = '#e6d18c',
+    foreground = '#e6d18c',
+    operator   = '#b3a16d',
+    keyword    = '#5F865F',
+    class      = '#96a65e',
+    func       = '#96a65e',
+    string     = '#B3854D',
+    number     = '#B3664D',
+    error      = '#B3664D',
+    warning    = '#C9A654',
+    hint       = '#669977',
+}
+
+-- (defcolor fg              "#D7C484")
+-- (defcolor bg              "#24211E")
+-- (defcolor green           "#77824A")
+-- (defcolor teal            "#5F865F")
+-- (defcolor orange          "#BB7844")
+-- (defcolor red             "#B3664D")
+-- (defcolor op              "#669977")
+-- (defcolor str             "#B3854D")
+-- (defcolor cmt             "#6E665E")
+-- (defcolor delim           "#80744D")
+-- (defcolor altBg           "#212121")
+-- (defcolor darkOrange      "#B36B42")
+--
+-- (defcolor darkGreen       "#252F1E")
+-- (defcolor darkGreenAlt    "#444A2B")
+-- (defcolor lightBrown      "#463939")
+-- (defcolor diffChange      "#675642")
+-- (defcolor nonText         "#7A6D52")
+-- (defcolor indent          "#252F1E")
+-- (defcolor tablineBg       "#121212")
+-- (defcolor altLineNr       "#736659")
+-- (defcolor ignore          "#484441")
+-- (defcolor scroll          "#6B6461")
+-- (defcolor visual          "#3B3330")
+--
+-- (defcolor diagnosticOk    "#77824A")
+-- (defcolor diagnosticHint  "#6E665E")
+-- (defcolor diagnosticInfo  "#C9A654")
+-- (defcolor diagnosticWarn  "#BB7844")
+-- (defcolor diagnosticError "#B3664D")
+
+return palette
