a1f7ebef495d0b31dc0d10c28fa9926ccb8fe4cc
ellisra <robandrewellis@gmail.com>
2026-01-23 11:55:21 +0000

    Added ' pairing for f, r, and b strings

--- nvim/.config/nvim/after/ftplugin/python.lua
+++ nvim/.config/nvim/after/ftplugin/python.lua
@@ -1,3 +1,5 @@
+local mini_pairs = require('mini.pairs')
+
 vim.api.nvim_create_user_command('ISort', function(opts)
     local path = opts.args
     local isort = '!ruff check --select I --fix %s'
@@ -12,3 +14,15 @@ end, {
     complete = 'file',
     desc = 'isort current python file with ruff',
 })
+
+vim.keymap.set('i', "'", function()
+    local line = vim.api.nvim_get_current_line()
+    local col = vim.api.nvim_win_get_cursor(0)[2]
+    local char_before = col > 0 and line:sub(col, col) or ''
+
+    if char_before:match('[frb]') then
+        return mini_pairs.open("''", '..')
+    end
+
+    return mini_pairs.open("''", '[^%a\\].')
+end, { buffer = true, expr = true, replace_keycodes = false })
