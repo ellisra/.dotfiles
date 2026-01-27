620f2f91861e7e1a30d289ba1ddb719151d6ef9e
ellisra <robandrewellis@gmail.com>
2026-01-27 21:02:02 +0000

    Increased line height in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -36,7 +36,7 @@ cfg = {
     font = wez.font({ family = font_family, weight = weight_for_appearance(appearance) }),
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	font_size = 12,
-    line_height = 1.0,
+    line_height = 1.15,
 	bold_brightens_ansi_colors = 'No',
 	custom_block_glyphs = false,
 	hide_mouse_cursor_when_typing = true,
