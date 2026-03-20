c2670466d1bdf700c4d8f105363ee47f4f7deeb2
ellisra <robandrewellis@gmail.com>
2026-03-20 16:52:55 +0000

    Housekeeping

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
-vim.g.dark_default = 'habamax-aaa'
+vim.g.dark_default = 'modus'
 vim.g.light_default = 'dayfox'
 
 -- Lazy bootstrap
